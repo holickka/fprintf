ae28dff0edbb01430ffd1c10634f1698eeb47b90
holickka <64195032+holickka@users.noreply.github.com>
2023-11-12 12:32:45 +0800

    Add files via upload

---
+++ ft_printf.c
@@ -0,0 +1,37 @@
+#include "libftprintf.h"
+#include <stdarg.h>
+/*
+static void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+}
+
+int	ft_printf(const char *str ...)
+{
+	while (str[i])
+	{
+		if (str[i] == '%') 
+		
+		else
+			ft_putstr(str);
+			
+	}
+}
+*/
+#include <stdio.h>
+int	main()
+{
+	char s[] = "Whale";
+	printf("what %s\n", s);
+}
+/*
+|notes|
+printf("%s Mytext", myfunction1);
+*/
+
+
