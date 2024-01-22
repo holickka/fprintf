b325627b96a0c8ce74191ed532107a7cf619f07c
holickka <64195032+holickka@users.noreply.github.com>
2024-01-22 16:05:46 +0800

    Add files via upload
    updated

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int	main()
 {
-	int	y = ft_puthex(-1999, 'x');
-//	int	x = -1999;	
-//	int y = printf("%x", x);
-	printf("\n");
+//	int	y = ft_puthex(-1999, 'x');
+	int	x = -1999;	
+	int y = ft_printf("%x\n", x);
+	ft_printf("%%\n");
 	printf("%d\n", y);
 }
