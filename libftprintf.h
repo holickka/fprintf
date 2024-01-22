02638ffb3eced625227babc365885e400c35a4e7
holickka <64195032+holickka@users.noreply.github.com>
2024-01-22 16:05:28 +0800

    added printf to header file

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: hsim <hsim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 20:58:23 by hsim              #+#    #+#             */
-/*   Updated: 2023/11/20 15:24:36 by hsim             ###   ########.fr       */
+/*   Updated: 2024/01/22 15:49:51 by hsim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int		ft_putstr(char *s);
 int		ft_putptr(void *n);
 int		ft_putptr_hex(unsigned long long n, char c);
 size_t	ft_strlen(const char *s);
+int		ft_printf(const char *str, ...);
 
 #endif
